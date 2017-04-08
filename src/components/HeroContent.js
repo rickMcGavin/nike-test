2429d2e65f7d3e34d7810266517f344dab77afdf
rebelBeard <rfm144@gmail.com>
2017-04-08 12:03:24 -0400

    completed hero content for mobile

--- src/components/HeroContent.js
+++ src/components/HeroContent.js
@@ -3,23 +3,23 @@ import { StyleSheet, css } from 'aphrodite';
 
 const HeroContent = () => {
 	return (
-		<div className={css(styles.heroContent)}>
+		<div className={css(styles.heroContent, styles.heroContentSmall)}>
 			<div className={css(styles.spanContainer)}>
-				<span className={css(styles.span)}></span>
-				<span className={css(styles.span)}></span>
+				<span className={css(styles.span, styles.spanXSmall)}></span>
+				<span className={css(styles.span, styles.spanXSmall)}></span>
 			</div>
-			<div className={css(styles.contentRow)}>
-					<div className={css(styles.contentColumn)}>
+			<div className={css(styles.contentRow, styles.contentRowSmall, styles.contentRowXSmall)}>
+					<div className={css(styles.contentColumn, styles.contentColumnXSmall)}>
 						<h3 className={css(styles.h3, styles.h3MarginBottomAdjust)}>BLANDIT </h3>
 						<h3 className={css(styles.h3, styles.h3MarginTopAdjust)}>TINCIDUNT METUS</h3>
 						<p className={css(styles.p)}>Proin eget tortor malesuada, dapibus mi pretium, semper diam. Donec in rutrum turpis. Duis eu faucibus est. Aliquam vitae nibh vestibulum, rhoncus augue eu, venenatis erat. Vivamus ipsum metus, sodales nec scelerisque faucibus, cursus in quam. Mauris nibh neque, euismod in malesuada sed, commodo et lacus. Maecenas gravida, lacus dictum eleifend facilisis, justo felis aliquam dui, nec sodales quam neque</p>
 					</div>
-					<div className={css(styles.contentColumn)}>
+					<div className={css(styles.contentColumn, styles.contentColumnXSmall)}>
 						<h3 className={css(styles.h3, styles.h3MarginBottomAdjust)}>METUS</h3>
 						<h3 className={css(styles.h3, styles.h3MarginTopAdjust)}>PELLENTESQUE</h3>
 						<p className={css(styles.p)}>Proin eget tortor malesuada, dapibus mi pretium, semper diam. Donec in rutrum turpis. Duis eu faucibus est. Aliquam vitae nibh vestibulum, rhoncus augue eu, venenatis erat. Vivamus ipsum metus, sodales nec scelerisque faucibus, cursus in quam. Mauris nibh neque, euismod in malesuada sed, commodo et lacus. Maecenas gravida, lacus dictum eleifend facilisis, justo felis aliquam dui, nec sodales quam neque</p>
 					</div>
-					<div className={css(styles.contentColumn)}>
+					<div className={css(styles.contentColumn, styles.contentColumnXSmall)}>
 						<h3 className={css(styles.h3, styles.h3MarginBottomAdjust)}>BLANDIT</h3>
 						<h3 className={css(styles.h3, styles.h3MarginTopAdjust)}>TINCIDUNT METUS</h3>
 						<p className={css(styles.p)}>Proin eget tortor malesuada, dapibus mi pretium, semper diam. Donec in rutrum turpis. Duis eu faucibus est. Aliquam vitae nibh vestibulum, rhoncus augue eu, venenatis erat. Vivamus ipsum metus, sodales nec scelerisque faucibus, cursus in quam. Mauris nibh neque, euismod in malesuada sed, commodo et lacus. Maecenas gravida, lacus dictum eleifend facilisis, justo felis aliquam dui, nec sodales quam neque</p>
@@ -34,7 +34,14 @@ const styles = StyleSheet.create ({
 	heroContent: {
 		backgroundColor: '#17171C',
 		width: '100%',
-		minHeight: '75vh'
+		minHeight: '75vh',
+		padding: '0 15.8%',
+	},
+
+	heroContentSmall: {
+		'@media (max-width: 800px)': {
+			padding: '0 7.9%'
+		}
 	},
 
 	spanContainer: {
@@ -50,16 +57,40 @@ const styles = StyleSheet.create ({
 		backgroundColor: '#202127'
 	},
 
+	spanXSmall: {
+		'@media (max-width: 500px)': {
+			marginTop: '35px'
+		}
+	},
+
 	contentRow: {
 		display: 'flex',
-		margin: '0 15.8%',
 		paddingTop: '43px'
+	},
+
+	contentRowSmall: {
+		'@media (max-width: 800px)': {
+			paddingTop: '21.5px'
+		}
+	},
+
+	contentRowXSmall: {
+		'@media (max-width: 500px)': {
+			flexWrap: 'wrap',
+			paddingBottom: '20px'
+		}
 	},
 
 	contentColumn: {
 		width: '33%',
 		marginRight: '10px',
 		paddingRight: '5px'
+	},
+
+	contentColumnXSmall: {
+		'@media (max-width: 500px)': {
+			width: '100%'
+		}
 	},
 
 	h3: {
